8fa652d43926213bbd3bf08a2c08ec9c97570339
antomuangigitau <muangigitau@gmail.com>
2022-10-06 15:10:42 +0300

    Initial commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,96 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import styles from './Header.module.css';
+const Header = () => {
+  const [loading, setLoading] = useState(true);
+  const [input, setInput] = useState('');
+  const [items, setItems] = useState([]);
+  const API_KEY = process.env.REACT_APP_GEOLOCATION_API_KEY;
+  const url = 'https://geo.ipify.org/api/v2/country,city?apiKey=' + API_KEY;
+  const fetchMapImg = useCallback(async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(url);
+      const data = await response.json();
+      setLoading(false);
+      setItems(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [url]);
+  useEffect(() => {
+    fetchMapImg();
+    console.log('useRef');
+  }, []);
+  console.log(items);
+  const handleChange = () => {
+    if (input == null) return;
+    const foundIp = fetchMapImg(input);
+    setItems(foundIp);
+    setInput('');
+  };
+  return (
+    <header className={styles['header__title'] + ' ' + styles['header__flex']}>
+      <h3>IP Address Tracker</h3>
+      <div className={styles['search']}>
+        <div className={styles['search__bar']}>
+          <input
+            type='text'
+            placeholder='Search for any IP address or domain'
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+        </div>
+        <button onClick={handleChange} className={styles.btn}>
+          <svg xmlns='http://www.w3.org/2000/svg' width='11' height='14'>
+            <path fill='none' stroke='#FFF' strokeWidth='3' d='M2 1l6 6-6 6' />
+          </svg>
+        </button>
+      </div>
+
+      <div className={styles['report__box']}>
+        {loading && (
+          <div className={styles['report__spacing']}>
+            <div className={styles['report']}>
+              <h4>IP Address</h4>
+              <p>-</p>
+            </div>
+            <div className={styles['report']}>
+              <h4>Location</h4>
+              <p>-</p>
+            </div>
+            <div className={styles['report']}>
+              <h4>Timezone</h4>
+              <p>-</p>
+            </div>
+            <div className={styles['report']}>
+              <h4>Isp</h4>
+              <p>-</p>
+            </div>
+          </div>
+        )}
+        {!loading && (
+          <div className={styles['report__spacing']}>
+            <div className={styles['report']}>
+              <h4>IP Address</h4>
+              <p>{items.ip}</p>
+            </div>
+            <div className={styles['report']}>
+              <h4>Location</h4>
+              <p>{`${items.location.city}, ${items.location.country}`}</p>
+            </div>
+            <div className={styles['report']}>
+              <h4>Timezone</h4>
+              <p>{`UTC - ${items.location.timezone.slice(1)}`}</p>
+            </div>
+            <div className={styles['report']}>
+              <h4>Isp</h4>
+              <p>{items.isp}</p>
+            </div>
+          </div>
+        )}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/main/Main.js
@@ -0,0 +1,44 @@
+import React, { useRef, useEffect } from 'react';
+import L from 'leaflet';
+
+const Main = () => {
+  const mapRef = useRef(null);
+  const tileRef = useRef(null);
+  tileRef.current = L.tileLayer(
+    'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }
+  );
+  const mapParams = {
+    center: [-1.066495, 37.05213],
+    zoom: 13,
+    zoomControl: false,
+    // maxBounds: L.latLngBounds(L.latLng(-150, -240), L.latLng(150, 240)),
+    layers: [tileRef.current],
+  };
+  // Define the styles that are to be passed to the map instance:
+  const mapStyles = {
+    overflow: 'hidden',
+    width: '100%',
+    height: '100vh',
+  };
+  useEffect(() => {
+    var container = L.DomUtil.get('map');
+
+    if (container != null) {
+      container._leaflet_id = null;
+    }
+    mapRef.current = L.map('map', mapParams);
+  });
+
+  return (
+    <main>
+      <div id='map' style={mapStyles}></div>
+    </main>
+  );
+};
+
+export default Main;
